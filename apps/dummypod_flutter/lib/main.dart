d46ec35a7b7e9adbe6ff406ce1fb1943e17908b2
Dhafin Rayhan <dhafindiamle@gmail.com>
2024-04-04 14:10:57 +0700

    Fix analysis problems

--- apps/dummypod_flutter/lib/main.dart
+++ apps/dummypod_flutter/lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:dummypod_client/dummypod_client.dart';
-import 'package:flutter/material.dart';
-import 'package:serverpod_flutter/serverpod_flutter.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flextras/flextras.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:gap/gap.dart';
+import 'package:serverpod_flutter/serverpod_flutter.dart';
 
 // Sets up a singleton client object that can be used to talk to the server from
 // anywhere in our app. The client is generated from your server code.
