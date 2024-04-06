0d09adb207c8bbfc7345619ca67ba33eab51e7cf
Dhafin Rayhan <dhafindiamle@gmail.com>
2024-04-06 15:15:51 +0700

    Complete update current user fields

--- apps/dummypod_flutter/lib/users/update_current_user.dart
+++ apps/dummypod_flutter/lib/users/update_current_user.dart
@@ -15,6 +15,17 @@ class UpdateCurrentUserScreen extends HookWidget {
 
     final fullNameController = useTextEditingController();
     final ageController = useTextEditingController();
+    final gender = useState<Gender?>(null);
+    final phoneController = useTextEditingController();
+    final birthDate = useState<DateTime?>(null);
+    final bloodGroupController = useTextEditingController();
+    final heightController = useTextEditingController();
+    final weightController = useTextEditingController();
+    final eyeColorController = useTextEditingController();
+    final hairColorController = useTextEditingController();
+    final hairTypeController = useTextEditingController();
+    final domainController = useTextEditingController();
+    final ipController = useTextEditingController();
 
     Future<void> fetch() async {
       user.value = await client.users.getCurrentUser();
@@ -31,6 +42,17 @@ class UpdateCurrentUserScreen extends HookWidget {
       if (u != null) {
         fullNameController.text = u.userInfo?.fullName ?? '';
         ageController.text = u.age?.toString() ?? '';
+        gender.value = u.gender;
+        phoneController.text = u.phone ?? '';
+        birthDate.value = u.birthDate;
+        bloodGroupController.text = u.bloodGroup ?? '';
+        heightController.text = u.height?.toString() ?? '';
+        weightController.text = u.weight?.toString() ?? '';
+        eyeColorController.text = u.eyeColor ?? '';
+        hairColorController.text = u.hair?.color ?? '';
+        hairTypeController.text = u.hair?.type ?? '';
+        domainController.text = u.domain ?? '';
+        ipController.text = u.ip ?? '';
       }
 
       return null;
@@ -44,6 +66,30 @@ class UpdateCurrentUserScreen extends HookWidget {
           age: ageController.text.isNotEmpty
               ? int.parse(ageController.text)
               : null,
+          gender: gender.value,
+          phone: phoneController.text.isNotEmpty ? phoneController.text : null,
+          birthDate: birthDate.value,
+          bloodGroup: bloodGroupController.text.isNotEmpty
+              ? bloodGroupController.text
+              : null,
+          height: heightController.text.isNotEmpty
+              ? int.parse(heightController.text)
+              : null,
+          weight: weightController.text.isNotEmpty
+              ? double.parse(weightController.text)
+              : null,
+          eyeColor: eyeColorController.text.isNotEmpty
+              ? eyeColorController.text
+              : null,
+          hair: (hairColorController.text.isNotEmpty &&
+                  hairTypeController.text.isNotEmpty)
+              ? Hair(
+                  color: hairColorController.text,
+                  type: hairTypeController.text)
+              : null,
+          domain:
+              domainController.text.isNotEmpty ? domainController.text : null,
+          ip: ipController.text.isNotEmpty ? ipController.text : null,
         ),
         fullName:
             fullNameController.text.isNotEmpty ? fullNameController.text : null,
@@ -72,6 +118,69 @@ class UpdateCurrentUserScreen extends HookWidget {
                 controller: ageController,
                 decoration: const InputDecoration(labelText: 'Age'),
                 keyboardType: TextInputType.number,
+              ),
+              ItemPickerField<Gender>(
+                value: gender.value,
+                onChanged: (value) => gender.value = value,
+                items: Gender.values,
+                labelText: 'Gender',
+              ),
+              TextField(
+                controller: phoneController,
+                decoration: const InputDecoration(labelText: 'Phone'),
+                keyboardType: TextInputType.phone,
+              ),
+              DatePickerField(
+                value: birthDate.value,
+                onChanged: (value) => birthDate.value = value,
+                labelText: 'Birth date',
+              ),
+              TextField(
+                controller: bloodGroupController,
+                decoration: const InputDecoration(labelText: 'Blood group'),
+              ),
+              TextField(
+                controller: heightController,
+                decoration: const InputDecoration(labelText: 'Height'),
+                keyboardType: TextInputType.number,
+              ),
+              TextField(
+                controller: weightController,
+                decoration: const InputDecoration(labelText: 'Weight'),
+                keyboardType:
+                    const TextInputType.numberWithOptions(decimal: true),
+              ),
+              TextField(
+                controller: eyeColorController,
+                decoration: const InputDecoration(labelText: 'Eye color'),
+              ),
+              SeparatedRow(
+                separatorBuilder: () => const Gap(16),
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: hairColorController,
+                      decoration:
+                          const InputDecoration(labelText: 'Hair color'),
+                    ),
+                  ),
+                  Expanded(
+                    child: TextField(
+                      controller: hairTypeController,
+                      decoration: const InputDecoration(labelText: 'Hair type'),
+                    ),
+                  ),
+                ],
+              ),
+              TextField(
+                controller: domainController,
+                decoration: const InputDecoration(labelText: 'Domain'),
+                keyboardType: TextInputType.url,
+              ),
+              TextField(
+                controller: ipController,
+                decoration: const InputDecoration(labelText: 'IP'),
+                keyboardType: TextInputType.url,
               ),
               FilledButton(
                 onPressed: callEndpoint,
--- apps/dummypod_flutter/lib/widgets.dart
+++ apps/dummypod_flutter/lib/widgets.dart
@@ -47,3 +47,120 @@ bool _canConverToObject(String jsonString) {
     return false;
   }
 }
+
+class DatePickerField extends StatelessWidget {
+  const DatePickerField({
+    super.key,
+    required this.value,
+    required this.onChanged,
+    this.labelText,
+  });
+
+  final DateTime? value;
+  final ValueChanged<DateTime?>? onChanged;
+  final String? labelText;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      onTap: () async {
+        final result = await showDatePicker(
+          context: context,
+          firstDate: DateTime(1900),
+          lastDate: DateTime.now(),
+          initialDate: value,
+        );
+
+        if (result != null) {
+          onChanged?.call(result.add(result.timeZoneOffset).toUtc());
+        }
+      },
+      decoration: InputDecoration(
+        labelText: labelText,
+        hintText: value?.toIso8601String().substring(0, 10),
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        prefixIcon: const Icon(Icons.calendar_today),
+        suffixIcon: value != null
+            ? IconButton(
+                onPressed: () => onChanged?.call(null),
+                icon: const Icon(Icons.close),
+              )
+            : null,
+      ),
+      readOnly: true,
+    );
+  }
+}
+
+class ItemPickerField<T> extends StatelessWidget {
+  const ItemPickerField({
+    super.key,
+    required this.value,
+    required this.onChanged,
+    required this.items,
+    this.labelText,
+  });
+
+  final T? value;
+  final ValueChanged<T?>? onChanged;
+  final List<T> items;
+  final String? labelText;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      onTap: () async {
+        final result = await showDialog<T>(
+          context: context,
+          builder: (_) => _ItemPickerFieldDialog<T>(
+            items: items,
+            selectedItem: value,
+          ),
+        );
+
+        if (result != null) {
+          onChanged?.call(result);
+        }
+      },
+      decoration: InputDecoration(
+        labelText: labelText,
+        hintText: value?.toString(),
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: value != null
+            ? IconButton(
+                onPressed: () => onChanged?.call(null),
+                icon: const Icon(Icons.close),
+              )
+            : null,
+      ),
+      readOnly: true,
+    );
+  }
+}
+
+class _ItemPickerFieldDialog<T> extends StatelessWidget {
+  const _ItemPickerFieldDialog({
+    required this.items,
+    this.selectedItem,
+  });
+
+  final List<T> items;
+  final T? selectedItem;
+
+  @override
+  Widget build(BuildContext context) {
+    return SimpleDialog(
+      clipBehavior: Clip.antiAlias,
+      children: [
+        for (final item in items)
+          ListTile(
+            onTap: () => Navigator.of(context).pop(item),
+            title: Text(
+              item.toString(),
+            ),
+            trailing: item == selectedItem ? const Icon(Icons.check) : null,
+          )
+      ],
+    );
+  }
+}
